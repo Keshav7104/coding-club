63f42d69de0afc53a4f3ea448bef2b4f932eb2c8
Vanshika Marwaha <vmarwaha02@gmail.com>
2023-02-16 16:32:24 +0530

    made the components folder and added footer file, here keshav and sachin can add navbar and layout

---
+++ src/Components/Footer.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import './footer.css';
+
+export default function Footer () {  
+    return(
+<>
+    {/* <section>Footer Example 4</section> */}
+	<div class="down">
+<footer class="footer-distributed">
+
+			<div class="footer-left">
+
+				<h3>Coding<span>Pundits</span></h3>
+
+				<p class="footer-links">
+					<a href="#" class="link-1">Home </a>
+				
+					<a href="#">Documentation </a>
+				
+					<a href="#">Events </a>
+					
+					<a href="#">Our Team </a>
+					
+					<a href="#">Alumni </a>
+
+					<a href="#">Register</a>
+				</p>
+
+				<p class="footer-company-name">Coding Pundits © 2023</p>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>Jote, District:Papum Pare
+ </span> Arunachal Pradesh,<span>India - 791113</span> </p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p> +91 0360-2954549</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:nitapadmin@nitap.ac.in">nitapadmin@nitap.ac.in</a></p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>About the Club</span>
+					Coding Pundits aims to establish a coding culture on campus, reaching every student passionate about coding.<br></br> Here you'll be able to collaborate with others, learn from them and indeed teach them things you know. 
+				</p>
+				</div>
+				<div class="footer-icons">
+					<a href="#"><i class="fab fa-facebook-f"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-linkedin"></i></a>
+					<a href="#"><i class="fa fa-github"></i></a>
+
+
+			</div>
+			
+		</footer>
+		</div>
+		
+        </>
+)}
+
