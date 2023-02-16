5ee91deb607f7c6c5e7f82b774923dcad0849fdd
Vanshika Marwaha <vmarwaha02@gmail.com>
2023-02-16 17:16:09 +0530

    adding the background gif for footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
+import Footer from './Components/Footer';
 import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       <h1>nit ap coding club</h1>
     </div>
+    <Footer/>
+    </>
   );
 }
 
