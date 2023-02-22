0552a8f82622acd2a6c5c16df7f9ee151af80c9e
Vanshika Marwaha <vmarwaha02@gmail.com>
2023-02-22 16:11:54 +0530

    resolved merge conflicts in app.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,19 @@ import Nav  from './Components/layout/navbar';
 import Footer from './Components/layout/Footer';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import './App.css';
-// import Events from './Pages/Events';
 
+// import Events from './Pages/Events';
 
 function App() {
 
   return (
       <Router>
       <Nav></Nav>
-<<<<<<< HEAD
-      <section>
-        ednekjnfk
-      </section>
+    
       {/* <Events></Events> */}
-=======
       <Routes>
         <Route path='/'/>
       </Routes>
->>>>>>> 59c35ebd667e6c03dd5ae32083458a7a7bce5f34
       <Footer></Footer>
       </Router>
   );
