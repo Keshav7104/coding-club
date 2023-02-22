8a6289924898e834825de376aa471d7dd17a12b2
Vanshika Marwaha <vmarwaha02@gmail.com>
2023-02-22 16:02:57 +0530

    adding the events page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Nav  from './Components/layout/navbar';
 import Footer from './Components/Footer';
 import './App.css';
+// import Events from './Pages/Events';
 
 function App() {
   return (
     <div >
       <Nav></Nav>
+      <section>
+        ednekjnfk
+      </section>
+      {/* <Events></Events> */}
       <Footer></Footer>
     </div>
   );
