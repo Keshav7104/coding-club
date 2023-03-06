33f7ff2f494c00f2ae7718bd12dec326ec1b73bc
Keshav <keshavbhutaani1360@gmail.com>
2023-03-07 01:02:45 +0530

    so i amde a page for admin which will show after login in which you can see current admins add admins remove admins and change your loging in password also i have added comments to some files to get better understanding while reading them

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import Nav  from './Components/layout/navbar';
-import Footer from './Components/layout/Footer';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import Nav  from './Components/layout/navbar'; //navbar for every page
+import Footer from './Components/layout/Footer';// footer for every page
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';//for making one page without loading go to other page
+import { useState,createContext } from 'react';//to pass information among pages
 import Register from './Pages/Register';
 import Event1 from './Pages/Event1';
-import { useState,createContext } from 'react';
-import Home from './Pages/Home'
+import Home from './Pages/Home';
+import Admin from './Pages/Admin';//different pages of our site
 
-export const Admin = createContext();
+export const Info = createContext();//context to pass only those value which are meant to used in that page not all info
 
 function App() {
 
-  const [isadmin, setIsadmin] =useState(0);
-  // setIsadmin(0);
+  const [isadmin, setIsadmin] =useState(0);//0 if user is not admin 1 if user is admin
+  const [id,setId] = useState("");//to hold the value of admin id
   return (
-    <div>
-      <Admin.Provider value={{isadmin, setIsadmin}}>
+    <>
+      <Info.Provider value={{isadmin, setIsadmin,id,setId}}>
       <Router>
       <Nav />
       <Routes>
@@ -22,13 +23,15 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path="/register" element={<Register />}/>
         <Route path="/events" element={<Event1 />}/>
+        <Route path='/Admin' element={<Admin />} />
+        <Route path='*' element={<h1>Sorry,you connected to wrong wire</h1>} />
         
       </Routes>
 
       <Footer></Footer>
       </Router>
-      </Admin.Provider>
-      </div>
+      </Info.Provider>
+      </>
   );
 }
 
