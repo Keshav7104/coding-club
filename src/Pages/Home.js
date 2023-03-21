22e1558cc72fa1078ea6a2c7b90865eb65b8db6c
Sachin Pathak <pathaksachin876@gmail.com>
2023-03-21 12:21:02 +0530

    added files

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,9 @@
 import About from '../Components/Cards/About';
-
+import Animation from './Animation.css';
 export default function Home (){
     return(
         <>
+       <Animation />
         <About />
         </>
     )
