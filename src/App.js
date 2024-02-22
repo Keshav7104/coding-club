1423937ae8258f4506855533aa2bc51736d897a2
Keshav <keshavbhutaani1360@gmail.com>
2024-02-22 18:27:25 +0530

    Added Visitors count in footer

--- src/App.js
+++ src/App.js
@@ -5,9 +5,19 @@ import Register from "./Pages/Register";
 import Event1 from "./Pages/Event1";
 import Home from "./Pages/Home";
 import News from "./Pages/News";
+import { useEffect, useState } from "react";
 
 
 function App() {
+  const [Visitors, setVisitors] = useState(0);
+
+  useEffect(()=>{
+    const storedCount = localStorage.getItem("visitors");
+    const initialCount = Number(storedCount) || 0;
+    setVisitors(initialCount+1);
+    localStorage.setItem("visitors",initialCount+1);
+  },[])
+
   return (
     <>
       <Router>
@@ -19,10 +29,8 @@ function App() {
           <Route path="/News" element={<News />} />
           <Route path="*" element={<h1>Sorry,you connected to wrong wire</h1>} />
         </Routes>
-
-        <Footer></Footer>
+        <Footer visitors={Visitors} />
       </Router>
-      <footer></footer>
     </>
   );
 }
